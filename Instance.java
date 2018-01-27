f92d63576a65d3e926e6b8308f7919fc1fb8833e
Kimaya Desai <kimaya.desai@gmail.com>
2018-01-27 12:09:16 -0800

    Update Instance.java

--- Instance.java
+++ Instance.java
@@ -1,52 +1,47 @@
 
-class Motorcycle{
-
+class Motorcycle {
 	// instance variables
 	String make;
 	String color;
 	boolean engineState;
 
-    // instance behaviour defined by methods
+   	 // instance behaviour defined by methods
 	void startEngine(){
 		if (engineState == true) {
 			System.out.println("Started already");
 		}
-		else{
+		else {
 			engineState = true;
 			System.out.println("Started now");
 		}
 	}
 
 	void showAtts() {
-    System.out.println("This motorcycle is "+ color + " "  + make);
-    if (engineState == true)
-        System.out.println("The engine is on.");
-    else 
-    	System.out.println("The engine is off.");
-  }
-
-
+    		System.out.println("This motorcycle is "+ color + " "  + make);
+    		if (engineState == true)
+       			 System.out.println("The engine is on.");
+    		else 
+    			System.out.println("The engine is off.");
+  	}
 }
 
-class Instance{
+class Instance {
 	public static void main(String args[]){
-		System.out.println("Hello World");
-
+		
 		Motorcycle m = new Motorcycle();
 		m.make = "Yamaha RZ350";
-        m.color = "yellow";
-        System.out.println("Calling showAtts...");
-        m.showAtts();
+        	m.color = "yellow";
+        	System.out.println("Calling showAtts...");
+        	m.showAtts();
 
-        System.out.println("Starting engine...");
-        m.startEngine();
+        	System.out.println("Starting engine...");
+        	m.startEngine();
 
-        System.out.println("Calling showAtts...");
-        m.showAtts();
+        	System.out.println("Calling showAtts...");
+        	m.showAtts();
 
 		System.out.println("Starting engine...");
 		m.startEngine();
 
-	}
-	
+	}	
 }
